005647938f34afa79e12ec8cb84a212e3d523608
Alex-J-W <alex.jauchwalser@gmail.com>
2022-12-03 18:48:19 +0100

    feat: find all bars

---
+++ api/controllers/dashboard/view-landing.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  friendlyName: 'View page landing',
+
+  description: 'Display "Landing" page with fetched bars.',
+
+  exits: {
+    success: {
+      viewTemplatePath: 'pages/homepage'
+    }
+
+  },
+
+  fn: async () => {
+    // TODO: filter for only 6 bars
+    let bars = await sails.helpers.getAllBars();
+    return {bars};
+  }
+};
