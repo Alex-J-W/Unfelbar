dc3ccdf48c292d7a32d0ab6bfc8d75d4c3b3b21e
Alex-J-W <alex.jauchwalser@gmail.com>
2023-01-18 15:53:18 +0100

    updated columns

--- api/models/Ticket.js
+++ api/models/Ticket.js
@@ -1,10 +1,7 @@
-// TODO: fix columnType if error
 module.exports = {
   attributes: {
-    eventName: { type: 'string', required: true },
-    eventDate: { type: 'string', columnType: 'date', required: true },
-    price: { type: 'number', required: true },
-    event: { model: 'Barevent', required: true },
+    number: { type: 'number', required: true },
+    barEvent: { model: 'Barevent', required: true },
     customer: { model: 'User', required: true},
   },
 };
