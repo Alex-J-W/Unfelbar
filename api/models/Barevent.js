1bc8432fbb7b820ab7f893f38a8638e4e2dcf012
Alex-J-W <alex.jauchwalser@gmail.com>
2023-01-18 15:44:50 +0100

    Updated

--- api/models/Barevent.js
+++ api/models/Barevent.js
@@ -2,8 +2,10 @@ module.exports = {
   attributes: {
     name: { type: 'string', required: true },
     description: { type: 'string', required: true },
-    capacity: { type: 'number', required: true },
-    date: { type: 'string', columnType: 'date', required: true},
+    ticketVolume: { type: 'number', required: true },
+    ticketsSold: { type: 'number', required: true },
+    date: { type: 'ref', columnType: 'DATE', required: true},
+    price:{ type: 'number', required: true },
     bar: { model: 'Bar', required: true }
   },
 };
