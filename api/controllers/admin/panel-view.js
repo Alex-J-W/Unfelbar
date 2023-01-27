fd05df9387cab311521de17b285b7d399da1facc
Alex-J-W <alex.jauchwalser@gmail.com>
2023-01-27 13:48:58 +0100

    added admin page

---
+++ api/controllers/admin/panel-view.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+
+  friendlyName: 'Panel view',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+    success: {
+      description: 'Get panel template',
+      viewTemplatePath: 'pages/admin/panel'
+    }
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
