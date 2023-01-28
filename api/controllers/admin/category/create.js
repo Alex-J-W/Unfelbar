6fa6446074c20a38419e040b6fc2c4745d75fc3e
Alex-J-W <alex.jauchwalser@gmail.com>
2023-01-28 11:05:17 +0100

    fix

--- api/controllers/admin/category/create.js
+++ api/controllers/admin/category/create.js
@@ -2,16 +2,15 @@ module.exports = {
 
   friendlyName: 'Category create',
 
-  // TODO Change bar item name
-  description: 'Creates a category for a bar item',
+  description: 'Creates a category for a menuItem',
 
   inputs: {
     name: { type: 'string', allowNull: false, required: true }
   },
 
   exits: {
-    redirect: {
-      description: 'Created new Bar - redirect to category overview',
+    success: {
+      description: 'Created new Category - redirect to category overview',
       responseType: 'redirect'
     }
   },
@@ -19,7 +18,6 @@ module.exports = {
   fn: async ({name}) => {
     await Category.create({name});
 
-    // TODO find better way to manage redirect
-    throw { redirect: '/category/index' };
+    return '/category/index';
   }
 };
